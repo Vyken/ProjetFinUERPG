786777f1fef649f592f322d728b5cb789f1e8b18
Vyken <kenan.potacsek@hotmail.com>
2023-02-13 17:50:09 +0100

    exposition de l'API par ajout des API ressource

--- src/Entity/Personnage.php
+++ src/Entity/Personnage.php
@@ -2,11 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\PersonnageRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+
 #[ORM\Entity(repositoryClass: PersonnageRepository::class)]
+#[ApiResource()]
 class Personnage
 {
     #[ORM\Id]
